2c4a9db2d079783f4cf55241180e6d849c3fd8d1
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-02 18:19:10 -0300

    check if root element exists in html layout, and only after that render component app

--- part1/example-part1/src/index.js
+++ part1/example-part1/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("The element root was not found");
+}
+
+ReactDOM.createRoot(rootElement).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
