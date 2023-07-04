a8384917d63536f3a63f65a2eb866fe0d8950e97
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-04 10:44:14 -0300

    finished exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,44 @@
 import React from "react";
 
 /**
+ * @param {Object} props
+ * @returns {import("react").ReactElement}
+ */
+const Header = props => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+/**
+ * @param {Object} props
+ * @returns {import("react").ReactElement}
+ */
+const Content = props => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+/**
+ * @param {Object} props
+ * @returns {import("react").ReactElement}
+ */
+const Total = props => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  );
+};
+
+/**
  * @returns {import("react").ReactElement}
  */
 const App = () => {
@@ -14,17 +52,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
