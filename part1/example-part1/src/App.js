a7f6152048608b64ee4806aab7ea310eb15ddbbb
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-19 10:05:52 -0300

    finished component helper functions exercise

--- part1/example-part1/src/App.js
+++ part1/example-part1/src/App.js
@@ -16,13 +16,20 @@ const Footer = () => {
  * @returns {import("react").ReactElement}
  */
 const Hello = props => {
-  console.log(props);
+  /**
+   * @returns {Number}
+   */
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
 
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
