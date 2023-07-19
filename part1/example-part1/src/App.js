3cb69a8bed3d7b1f594246b0b3e9ced4253cc8fd
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-19 10:15:25 -0300

    finished destructuring exercise

--- part1/example-part1/src/App.js
+++ part1/example-part1/src/App.js
@@ -15,19 +15,16 @@ const Footer = () => {
  * @param {Object} props
  * @returns {import("react").ReactElement}
  */
-const Hello = props => {
+const Hello = ({ name, age }) => {
   /**
    * @returns {Number}
    */
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
+  const bornYear = () => new Date().getFullYear() - age;
 
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
