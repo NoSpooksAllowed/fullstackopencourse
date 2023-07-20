3f678643c408acbe308a48bd67cce97b82bab4c0
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-20 15:08:12 -0300

    finished complex state exercise

--- part1/example-part1/src/App.js
+++ part1/example-part1/src/App.js
@@ -50,18 +50,21 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
  * @returns {import("react").ReactElement}
  */
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+
+  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 });
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   );
 };
