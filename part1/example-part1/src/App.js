e06a1c5827822e06693c31fbb0cdb1642a6bab23
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-20 16:20:57 -0300

    finished conditional rendering exercise

--- part1/example-part1/src/App.js
+++ part1/example-part1/src/App.js
@@ -47,6 +47,17 @@ const Display = ({ counter }) => {
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
 
 /**
+ * @param {Object} props
+ * @returns {import("react").ReactElement}
+ */
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+/**
  * @returns {import("react").ReactElement}
  */
 const App = () => {
@@ -54,20 +65,15 @@ const App = () => {
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
 
-  const [total, setTotal] = useState(0);
-
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
     const updatedLeft = left + 1;
     setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
     setRight(right + 1);
-
-    setTotal(left + right);
   };
 
   return (
@@ -76,9 +82,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(" ")}</p>
-
-      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
