11bc1beb2641232d3e9feb5cd816df54cd298489
NoSpooksAllowed <hersgori4@protonmail.com>
2023-07-20 15:56:25 -0300

    finished update of the state is asynchronous exercise

--- part1/example-part1/src/App.js
+++ part1/example-part1/src/App.js
@@ -52,17 +52,22 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
-
   const [allClicks, setAll] = useState([]);
+
+  const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
-    setLeft(left + 1);
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
     setRight(right + 1);
+
+    setTotal(left + right);
   };
 
   return (
@@ -71,8 +76,9 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-
       <p>{allClicks.join(" ")}</p>
+
+      <p>total {total}</p>
     </div>
   );
 };
