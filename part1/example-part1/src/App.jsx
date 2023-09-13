13fd5cf5fab6d7797af4b204a39386772d8f643e
NoSpooksAllowed <hersgori4@protonmail.com>
2023-09-13 15:40:45 -0300

    finished example part1 d

--- part1/example-part1/src/App.jsx
+++ part1/example-part1/src/App.jsx
@@ -41,26 +41,50 @@ const Display = ({ counter }) => {
  * @param {Object} props
  * @returns {React.ReactElement}
  */
-const Button = ({ text, handleClick }) => {
-  return <button onClick={handleClick}>{text}</button>;
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+
+/**
+ * @param {Object} props
+ * @returns {React.ReactElement}
+ */
+const History = props => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
 
 /**
  * @returns {React.ReactElement}
  */
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  /** @type Array<string> */
+  const arr = [];
+  const [allClicks, setAll] = useState(arr);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      {left}
+
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
