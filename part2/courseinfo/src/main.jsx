8d886e1670ad14e6ee115f7e45374b1ba0f82a3d
NoSpooksAllowed <hersgori4@protonmail.com>
2023-09-24 22:38:08 -0300

    init courseinfo part2 exercise

---
+++ part2/courseinfo/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("The element root was not found");
+}
+
+ReactDOM.createRoot(rootElement).render(<App />);
