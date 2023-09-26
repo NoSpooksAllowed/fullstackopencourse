0a640a4796f1e5b77fcbb6adc0ef07161cfdf3fe
NoSpooksAllowed <hersgori4@protonmail.com>
2023-09-26 10:15:42 -0300

    finished examples part2b Forms

--- part2/example-part2/src/App.jsx
+++ part2/example-part2/src/App.jsx
@@ -15,15 +15,53 @@ import Note from "./components/Note";
  * @param {Array<Note>} props.notes
  * @returns {React.ReactElement}
  */
-const App = ({ notes }) => {
+const App = props => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  /**
+   * @param {React.FormEvent<HTMLFormElement>} event
+   * @returns {void}
+   */
+  const addNote = event => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  /**
+   * @param {React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>} event - The change event object.
+   * @returns {void}
+   */
+  const handleNoteChange = event => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
+      </div>
       <ul>
-        {notes.map(note => (
+        {notesToShow.map(note => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
