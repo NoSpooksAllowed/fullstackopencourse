f23d34db984722167af5eb7e49b6d7480ee3b340
NoSpooksAllowed <hersgori4@protonmail.com>
2023-09-27 13:14:07 -0300

    finished exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
+/**
+ * @typedef {Object} Person
+ * @property {number} id
+ * @property {string} name
+ * @property {string} number
+ */
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  /**
+   * @type {Person[]}
+   */
+  const initialPersons = [];
+  const [persons, setPersons] = useState(initialPersons);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilterName, setNewFilterName] = useState("");
   const [filteredPersons, setFilteredPersons] = useState([...persons]);
   const [showFiltered, setShowFiltered] = useState(false);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then(response => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   /**
    * @param {React.FormEvent<HTMLFormElement>} event
