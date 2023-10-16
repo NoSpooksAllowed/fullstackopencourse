c98c07cdebe7b0dd02a2d66bdb046c8ef1735331
NoSpooksAllowed <hersgori4@protonmail.com>
2023-10-15 22:20:04 -0300

    finished exercise 3.20 and exercise 3.21

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -23,6 +23,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^(?:\d{2,3}-\d{7}|\d{3}-\d{8})$/.test(v);
+      },
+    },
     required: true,
   },
 });
