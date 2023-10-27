7d9b7d9f0861b9c83fc1f4efc38e27aa26590584
NoSpooksAllowed <hersgori4@protonmail.com>
2023-10-27 20:22:51 -0300

    finished exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -10,7 +10,24 @@ const totalLikes = blogs => {
   return blogs.reduce((sumLikes, blog) => sumLikes + blog.likes, 0);
 };
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const blogWithMostLikes = blogs.reduce((maxLikesBlog, blog) => {
+    return blog.likes > maxLikesBlog.likes ? blog : maxLikesBlog;
+  }, blogs[0]);
+
+  return {
+    title: blogWithMostLikes.title,
+    author: blogWithMostLikes.author,
+    likes: blogWithMostLikes.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
