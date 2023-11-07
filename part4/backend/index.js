8a724b7097530aa466957c4af0f4878424407b53
NoSpooksAllowed <hersgori4@protonmail.com>
2023-11-06 21:20:50 -0300

    finished example part4b first half

--- part4/backend/index.js
+++ part4/backend/index.js
@@ -1,22 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const notesRouter = require("./controllers/notes");
-const middleware = require("./utils/middleware");
 
-const app = express();
-const Note = require("./models/note");
-
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-app.use("/api/notes", notesRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-const PORT = config.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
--- part4/backend/models/note.js
+++ part4/backend/models/note.js
@@ -1,20 +1,6 @@
 const mongoose = require("mongoose");
-const config = require("../utils/config");
 
 mongoose.set("strictQuery", false);
-
-const url = config.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-  .then(result => {
-    console.log("connected to MongoDB");
-  })
-  .catch(error => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
 
 const noteSchema = new mongoose.Schema({
   content: {
