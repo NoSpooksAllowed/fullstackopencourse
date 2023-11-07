1cb0a21cf3bd036fa8b3594e66864e0157feb259
NoSpooksAllowed <hersgori4@protonmail.com>
2023-11-07 09:30:05 -0300

    finished exercise 4.8

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -16,6 +16,19 @@ beforeEach(async () => {
   }
 }, 100000);
 
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+}, 100000);
+
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(helper.initialBlogPosts.length);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
