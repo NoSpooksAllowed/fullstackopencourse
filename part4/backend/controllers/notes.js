292dfa1591407854ee8ab19b567dc8b7ab763156
NoSpooksAllowed <hersgori4@protonmail.com>
2024-02-10 11:33:24 -0300

    finished example part4d token authentication

--- part4/backend/controllers/notes.js
+++ part4/backend/controllers/notes.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
   const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
@@ -18,21 +27,28 @@ notesRouter.get("/:id", async (request, response, next) => {
   }
 });
 
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(
+    getTokenFrom(request),
+    String(process.env.SECRET)
+  );
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
-    user: user.id,
+    important: body.important === undefined ? false : body.important,
+    user: user._id,
   });
 
   const savedNote = await note.save();
   user.notes = user.notes.concat(savedNote._id);
   await user.save();
 
-  response.status(201).json(savedNote);
+  response.json(savedNote);
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
@@ -49,7 +65,9 @@ notesRouter.put("/:id", async (request, response, next) => {
     important: body.important,
   };
 
-  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true });
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+  });
 
   response.json(updatedNote);
 });
