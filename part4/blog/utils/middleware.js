0d929bda0861f448a191fffe4b59e958a897e065
NoSpooksAllowed <hersgori4@protonmail.com>
2024-02-28 18:45:01 -0300

    handle custom errors

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -22,9 +22,11 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "JsonWebTokenError") {
     return response.status(401).json({ error: error.message });
   } else if (error.name === "TokenExpiredError") {
-    return response.status(401).json({
-      error: "token expired",
-    });
+    return response.status(401).json({ error: "token expired" });
+  } else if (error.name === "FieldLengthError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "UniqueFieldError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
