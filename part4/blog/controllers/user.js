87259046b114549734553a8865d73cdb467e381b
NoSpooksAllowed <hersgori4@protonmail.com>
2024-02-28 18:46:10 -0300

    finished exercise 4.16

--- part4/blog/controllers/user.js
+++ part4/blog/controllers/user.js
@@ -1,10 +1,40 @@
 const bcrypt = require("bcrypt");
 const userRouter = require("express").Router();
 const User = require("../models/user");
+const {
+  ValidationError,
+  FieldLengthError,
+  UniqueFieldError,
+} = require("../utils/custom_errors");
 
-userRouter.post("/", async (request, response) => {
+userRouter.post("/", async (request, response, next) => {
   try {
     const { username, name, password } = request.body;
+
+    if (!password) {
+      throw new ValidationError("No field: password");
+    }
+
+    if (!username) {
+      throw new ValidationError("No field: username");
+    }
+
+    if (!name) {
+      throw new ValidationError("No field: name");
+    }
+
+    if (password.length < 4) {
+      throw new FieldLengthError("password should be more than 3 symbols long");
+    }
+
+    if (username.length < 4) {
+      throw new FieldLengthError("username should be more than 3 symbols long");
+    }
+
+    const userObject = await User.findOne({ username: username }).exec();
+    if (userObject) {
+      throw new UniqueFieldError("username should be unique");
+    }
 
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
@@ -20,17 +50,17 @@ userRouter.post("/", async (request, response) => {
 
     response.status(201).json(savedUser);
   } catch (error) {
-    response.status(400).send({ message: error.message });
+    next(error);
   }
 });
 
-userRouter.get("/", async (request, response) => {
+userRouter.get("/", async (request, response, next) => {
   try {
     const users = await User.find({});
 
     response.status(200).json(users);
   } catch (error) {
-    response.status(400).send({ message: error.message });
+    next(error);
   }
 });
 
